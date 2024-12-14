82b023b4e3585950a8e69c94640f174770543d73
Mario-MihaiMateaș <mateasmario@ibm.com>
2024-12-14 09:17:12 +0200

    First iteration

--- mockapi/src/main/java/com/docsbymario/mockapi/entity/impl/Faculty.java
+++
@@ -1,3 +0,0 @@
-package com.docsbymario.mockapi.entity.impl;
-
-public record Faculty(int id, String facultyName, String location) {}
---
+++ root/src/main/java/com/docsbymario/root/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.docsbymario.root.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ModelAndView getHome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("templates/index.html");
+        return modelAndView;
+    }
+}
