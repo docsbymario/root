4fbf82ef7bcddf5563b1458aa279ae3f76c9ab20
Mario-MihaiMateaș <mateasmario@ibm.com>
2024-12-18 13:47:05 +0200

    Fixed query parsing for apps

--- root/src/main/java/com/docsbymario/root/controller/HomeController.java
+++ root/src/main/java/com/docsbymario/root/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.docsbymario.root.controller;
 
 import com.docsbymario.root.entity.App;
 import com.docsbymario.root.service.AppService;
+import com.docsbymario.root.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,15 +24,22 @@ public class HomeController {
     @GetMapping("/apps")
     public ModelAndView getApps(@RequestParam(name="app-query", required=false) String appQuery,
                                 @RequestParam(name="app-query-mobile", required=false) String appQueryMobile) {
-        String query = appQueryMobile != null && !appQueryMobile.trim().isEmpty() ? appQueryMobile : appQuery;
+        String query = processQuery(appQuery, appQueryMobile);
 
         List<App> apps = appService.findAll().stream()
-                .filter(app -> app.getName().toLowerCase().contains(query.toLowerCase()) || app.getDescription().toLowerCase().contains(query.toLowerCase()))
+                .filter(app -> app.getName().toLowerCase().contains(query) || app.getDescription().toLowerCase().contains(query.toLowerCase()))
                 .distinct().toList();
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("apps.html");
         modelAndView.addObject("apps", apps);
         return modelAndView;
+    }
+
+    private String processQuery(String appQuery, String appQueryMobile) {
+        String result = !StringUtils.isEmpty(appQueryMobile) ? appQueryMobile :
+                !StringUtils.isEmpty(appQuery) ? appQuery : "";
+
+        return result.trim().toLowerCase();
     }
 }
---
+++ root/src/main/java/com/docsbymario/root/util/StringUtils.java
@@ -0,0 +1,7 @@
+package com.docsbymario.root.util;
+
+public class StringUtils {
+    public static boolean isEmpty(String input) {
+        return input == null || input.isEmpty();
+    }
+}
